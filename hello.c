d69f5846dbc0fc42e7e93b29e72cb3864eb1962d
Your Name <no@email>
2020-11-07 05:09:59 +0800

    Move message into a variable

--- hello.c
+++ hello.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+char *message = "Hello world!\n";
+
 int main(int argc, char *argv[]) {
     (void)argc; (void) argv;
-    printf("Hello world!\n");
+    printf(message);
     return 0;
 }
