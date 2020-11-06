8a7d8ef92a4e7dba4a2bea6fb1b55c420ae5a61c
Your Name <no@email>
2020-11-07 02:10:53 +0800

    Greet user if they give a name

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello uncertain worl\n");
+int main(int argc, char *argv[]) {
+    if (argc > 1) {
+        printf("Hello %s!\n", argv[1]);
+    } else {
+        printf("Hello uncertain worl\n");
+    }
     return 0;
 }
